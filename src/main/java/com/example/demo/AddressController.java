fae301d4077d4273a136d4607674ad2a8512e9b4
MayankSoni1024 <Mayank.soni1024@gmail.com>
2025-07-01 12:34:40 +0530

    Update AddressController.java

--- src/main/java/com/example/demo/AddressController.java
+++ src/main/java/com/example/demo/AddressController.java
@@ -3,12 +3,13 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+@RestController
 public class AddressController {
 
 	@Autowired
